6e3ae3742e5bc9c32fbd4cbe97f67e800e1e6ed1
SaMueLHsLie2D <168024601+SaMueLHsLie2D@users.noreply.github.com>
2025-03-12 18:34:51 -0300

    Update OperacoesMatematicas.java

--- src/OperacoesMatematicas.java
+++ src/OperacoesMatematicas.java
@@ -4,19 +4,16 @@ public class OperacoesMatematicas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita os números ao usuário
         System.out.print("Digite o primeiro número: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Digite o segundo número: ");
         double num2 = scanner.nextDouble();
 
-        // Realiza as operações
         double soma = num1 + num2;
         double subtracao = num1 - num2;
         double multiplicacao = num1 * num2;
 
-        // Exibe os resultados
         System.out.println("Soma: " + soma);
         System.out.println("Subtração: " + subtracao);
         System.out.println("Multiplicação: " + multiplicacao);
